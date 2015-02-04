87f0675125db86fffcf0ad4d1e8685ef0aa3d876
vitalie <vitalie@175134.no-reply.drupal.org>
2015-02-04 16:12:20 +0100

    Issue #2403885 by vitalie: The use_form functionality does not work

--- spamspan_admin.php
+++ spamspan_admin.php
@@ -39,70 +39,90 @@ class spamspan_admin {
     $filter->settings += $defaults;
   
     // spamspan '@' replacement
-    $settings['spamspan_at'] = array(
+    $form['spamspan_at'] = array(
       '#type' => 'textfield',
       '#title' => t('Replacement for "@"'),
       '#default_value' => $filter->settings['spamspan_at'],
       '#required' => TRUE,
       '#description' => t('Replace "@" with this text when javascript is disabled.'),
     );
-    $settings['spamspan_use_graphic'] = array(
+    $form['spamspan_use_graphic'] = array(
       '#type' => 'checkbox',
       '#title' => t('Use a graphical replacement for "@"'),
       '#default_value' => $filter->settings['spamspan_use_graphic'],
       '#description' => t('Replace "@" with a graphical representation when javascript is disabled'
         . ' (and ignore the setting "Replacement for @" above).'),
     );
-    $settings['spamspan_dot_enable'] = array(
+    $form['spamspan_dot_enable'] = array(
       '#type' => 'checkbox',
       '#title' => t('Replace dots in email with text'),
       '#default_value' => $filter->settings['spamspan_dot_enable'],
       '#description' => t('Switch on dot replacement.'),
     );
-    $settings['spamspan_dot'] = array(
+    $form['spamspan_dot'] = array(
       '#type' => 'textfield',
       '#title' => t('Replacement for "."'),
       '#default_value' => $filter->settings['spamspan_dot'],
       '#required' => TRUE,
       '#description' => t('Replace "." with this text.'),
     );
-    $settings['use_form'] = array(
+    $form['use_form'] = array(
       '#type' => 'fieldset',
       '#title' => t('Use a form instead of a link'),
     );
-    $settings['use_form']['spamspan_use_form'] = array(
+    $form['use_form']['spamspan_use_form'] = array(
       '#type' => 'checkbox',
       '#title' => t('Use a form instead of a link'),
       '#default_value' => $filter->settings['spamspan_use_form'],
-      '#description' => t('Link to a contact form instad of an email address.'
-        . ' The following settings are used only if you select this option.'),
+      '#description' => t('Link to a contact form instad of an email address. The following settings are used only if you select this option.'),
     );
-    $settings['use_form']['spamspan_use_url'] = array(
+    $form['use_form']['spamspan_form_pattern'] = array(
       '#type' => 'textfield',
       '#title' => t('Replacement string for the email address'),
-      '#default_value' => $filter->settings['spamspan_use_url'],
+      '#default_value' => $filter->settings['spamspan_form_pattern'],
       '#required' => TRUE,
-      '#description' => t('Replace the email link with this string and substitute the following <br />'
-        . '#formname = the webform name,<br />'
-        . '#email = the email address as typed,<br />'
-        . '#displaytext = text to display for the email address.'),
+      '#description' => t('Replace the email link with this string and substitute the following <br />%url = the url where the form resides,<br />%email = the email address (base64 and urlencoded),<br />%displaytext = text to display instead of the email address.'),
     );
-    $settings['use_form']['spamspan_email_encode'] = array(
-      '#type' => 'checkbox',
-      '#title' => t('Encode the email address'),
-      '#default_value' => $filter->settings['spamspan_email_encode'],
-      '#required' => TRUE,
-      '#description' => t('Encode the email address using base64 to protect from spammers.'
-        . ' Must be enabled for forms because the email address ends up in a URL.'),
-    );
-    $settings['use_form']['spamspan_email_default'] = array(
+    $form['use_form']['spamspan_form_default_url'] = array(
       '#type' => 'textfield',
-      '#title' => t('Default form'),
-      '#default_value' => $filter->settings['spamspan_email_default'],
+      '#title' => t('Default url'),
+      '#default_value' => $filter->settings['spamspan_form_default_url'],
       '#required' => TRUE,
-      '#description' => t('Default form to use if none specified'),
+      '#description' => t('Default url to form to use if none specified (e.g. me@example.com[custom_url_to_form])'),
     );
-    return $settings;
+    $form['use_form']['spamspan_form_default_displaytext'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Default displaytext'),
+      '#default_value' => $filter->settings['spamspan_form_default_displaytext'],
+      '#required' => TRUE,
+      '#description' => t('Default displaytext to use if none specified (e.g. me@example.com[custom_url_to_form|custom_displaytext])'),
+    );
+
+    // we need this to insert our own validate/submit handlers
+    // we use our own validate handler to extract use_form settings
+    $form['#process'] = array(
+      array($this, 'settings_form_process'),
+    );
+    return $form;
+  }
+
+  //attach our validation
+  public function settings_form_process(&$element, &$form_state, &$complete_form) {
+    $complete_form['#validate'][] = array($this, 'settings_form_validate');
+    return $element;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settings_form_validate(&$form, &$form_state) {
+    $settings = $form_state['values']['filters']['spamspan']['settings'];
+    $use_form = $settings['use_form'];
+
+    //no trees, see https://www.drupal.org/node/2378437
+    unset($settings['use_form']);
+    $settings += $use_form;
+    $form_state['values']['filters']['spamspan']['settings'] = $settings;
   }
 
   /**
@@ -165,36 +185,47 @@ class spamspan_admin {
         $settings = $this->filter->settings;
       }
     }
+
+    // processing for forms
+    if (!empty($settings['spamspan_use_form'])) {
+      $email = urlencode(base64_encode($name . '@' . $domain));
+
+      //put in the defaults if nothing set
+      if (empty($vars['custom_form_url'])) {
+        $vars['custom_form_url'] = $settings['spamspan_form_default_url'];
+      }
+      if (empty($vars['custom_displaytext'])) {
+        $vars['custom_displaytext'] = t($settings['spamspan_form_default_displaytext']);
+      }
+      $vars['custom_form_url'] = strip_tags($vars['custom_form_url']);
+      $vars['custom_displaytext'] = strip_tags($vars['custom_displaytext']);
+
+      $url_parts = parse_url($vars['custom_form_url']);
+      if (!$url_parts) {
+        $vars['custom_form_url'] = '';
+      }
+      else if (empty($url_parts['host'])) {
+        $vars['custom_form_url'] = base_path() . trim($vars['custom_form_url'], '/');
+      }
+
+      $replace = ['%url' => $vars['custom_form_url'], '%displaytext' => $vars['custom_displaytext'], '%email' => $email];
+
+      $output = strtr($settings['spamspan_form_pattern'], $replace);
+      return $output;
+    }
+
     $at = $settings['spamspan_at'];
     if ($settings['spamspan_use_graphic']) {
       $at = '<img class="spam-span-image" alt="at" width="10" src="' . base_path() . drupal_get_path('module', 'spamspan') . '/image.gif" />';
     }
-    
-    // New processing for forms.
-    if (isset($settings['spamspan_use_form']) and $settings['spamspan_use_form']) {
-      if ($settings['spamspan_email_encode']) {
-        $email = base64_encode($name . '@' . $domain) . '&en=1';
-      }
-      else {
-        $email = $email = $name . '@' . $domain;
-      }
-      $var = strip_tags($var);
-      if (strlen($var)) {
-        $vars = explode('|', $var . ' ');
-      }
-      if (!isset($vars[0]) or !strlen($vars[0])) {
-        $vars[0] = $settings['spamspan_email_default'];
-      }
-      $output = str_replace('#formname', $vars[0], $settings['spamspan_use_url']);
+
+    if (isset($settings['spamspan_dot_enable']) and $settings['spamspan_dot_enable']) {
+      // Replace .'s in the address with [dot]
+      $name = str_replace('.', '<span class="t">' . $settings['spamspan_dot'] . '</span>', $name);
+      $domain = str_replace('.', '<span class="t">' . $settings['spamspan_dot'] . '</span>', $domain);
     }
-    else {
-      if (isset($settings['spamspan_dot_enable']) and $settings['spamspan_dot_enable']) {
-        // Replace .'s in the address with [dot]
-        $name = str_replace('.', '<span class="t">' . $settings['spamspan_dot'] . '</span>', $name);
-        $domain = str_replace('.', '<span class="t">' . $settings['spamspan_dot'] . '</span>', $domain);
-      }
-      $output = '<span class="u">' . $name . '</span>' . $at . '<span class="d">' . $domain . '</span>';
-    }
+    $output = '<span class="u">' . $name . '</span>' . $at . '<span class="d">' . $domain . '</span>';
+
   
     // if there are headers, include them as eg (subject: xxx, cc: zzz)
     if (isset($headers) and $headers) {
